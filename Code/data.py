f7a7e91515ba21d5278135dbea6e369bb09719f5
Shravani Joshi <shravanijoshi0308y@gmail.com>
2026-06-28 12:11:09 +0200

    fix: slicing training set correctly to exclude validation data.

--- Code/data.py
+++ Code/data.py
@@ -7,22 +7,27 @@ import torch
 from pathlib import Path
 from torch.utils.data import TensorDataset, DataLoader
 
+# Takes the name of the dataset, loads it from the disk and spilt it into training, validation and test datasets.
 def get_loaders(data, data_path, batch_size, val_split=0.1):
     d_path = Path(data_path) / f"{data}_data.pt"
     data_dict = torch.load(d_path)
 
+# Calculating and setting the data into training and validation     total_samples = data_dict['train_images'].shape[0]
     total_samples = data_dict['train_images'].shape[0]
     val_size = int(total_samples * val_split)
     val_start = total_samples - val_size
-
-    train_data = data_dict['train_images']
-    train_labels = data_dict['train_labels']
+    
+    train_data = data_dict['train_images'][:val_start]
+    train_labels = data_dict['train_labels'][:val_start]
     val_data = data_dict['train_images'][val_start:]
     val_labels = data_dict['train_labels'][val_start:]
     
+    
+    # The data is already split into training and test datasets
+    # wrapping them into TensorDataset
     train_dataset = TensorDataset(train_data, train_labels)
     val_dataset = TensorDataset(val_data, val_labels)
-    test_dataset = TensorDataset(data_dict['test_images'], data_dict['test_labels'])
+    test_dataset = TensorDataset(data_dict['test_images'], data_dict['test_labels']) 
     
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
