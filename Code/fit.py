320c03f87f34d9be359d51e57fa1c9b9d474dc58
Shravani Joshi <shravanijoshi0308y@gmail.com>
2026-06-29 11:49:31 +0200

    Add zero_grad(); rename sum as total to stop shadowing

--- Code/fit.py
+++ Code/fit.py
@@ -19,7 +19,9 @@ class Trainer:
         
         for images, labels in dataloader:
             images, labels = images.to(self.device), labels.to(self.device)
-            
+            #Adding zero grad for the optimizer
+            self.optimizer.zero_grad()
+        
             outputs = self.model(images)
             loss = self.criterion(outputs, labels)
             
