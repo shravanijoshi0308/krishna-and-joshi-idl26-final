0c7fce803101d26f811a1942f9f982af8b89f6a1
Shravani Joshi <shravanijoshi0308y@gmail.com>
2026-06-29 12:44:40 +0200

    Fix: AlexNet Linear layer size from 2048 to 3072

--- Code/models.py
+++ Code/models.py
@@ -120,7 +120,7 @@ class VGG16(nn.Module):
         
         self.classifier = nn.Sequential(
             #nn.Linear(2048, 1024),
-            nn.Linear(3072, 1024)
+            nn.Linear(3072, 1024),
             nn.ReLU(inplace=True),
             nn.Dropout(p=drop_rate),
             nn.Linear(1024, 512),
