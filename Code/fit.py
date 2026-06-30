18e5135869b8839ba399e503875f261ec1cdf4d1
Shravani Joshi <shravanijoshi0308y@gmail.com>
2026-06-30 19:31:12 +0200

    feat: Extending evalute() to return percision, recall and macro F1

--- Code/fit.py
+++ Code/fit.py
@@ -4,6 +4,7 @@ MAI/IDL SS26 - Final assignment.
 MG 6/6/2026
 """
 import torch
+from sklearn.metrics import precision_score, recall_score, f1_score
 
 class Trainer:
     def __init__(self, model, criterion, optimizer, device):
@@ -42,6 +43,9 @@ class Trainer:
         self.model.eval()
         running_loss = 0.0
         correct, total = 0, 0
+        all_prediction = []
+        all_labels = []
+
         
         with torch.no_grad():
             for images, labels in dataloader:
@@ -54,8 +58,12 @@ class Trainer:
                 _, predicted = outputs.max(1)
                 total += labels.size(0)
                 correct += predicted.eq(labels).sum().item()
-                
-        return running_loss / total, (correct / total) * 100
+                all_prediction.extend(predicted.cpu().numpy())
+                all_labels.extend(labels.cpu().numpy())
+            precision = precision_score(all_labels, all_prediction, average='macro', zero_division=0)
+            recall = recall_score(all_labels, all_prediction, average='macro', zero_division=0)
+            f1 = f1_score(all_labels, all_prediction, average='macro', zero_division=0)
+        return running_loss / total, (correct / total) * 100 , precision, recall, f1
 
     def fit(self, train_loader, val_loader, epochs):
         print("\n Starting Training Routine...")
@@ -63,11 +71,12 @@ class Trainer:
         
         for epoch in range(epochs):
             train_loss, train_acc = self.train_one_epoch(train_loader)
-            val_loss, val_acc = self.evaluate(val_loader)
+            val_loss, val_acc, val_precision, val_recall, val_f1 = self.evaluate(val_loader)
             
             print(f"Epoch [{epoch+1:02d}/{epochs:02d}] | "
                   f"Train Loss: {train_loss:.4f} - Train Acc: {train_acc:.2f}% | "
-                  f"Val Loss: {val_loss:.4f} - Val Acc: {val_acc:.2f}%")
+                  f"Val Loss: {val_loss:.4f} - Val Acc: {val_acc:.2f}% | "
+                  f"Val Precision: {val_precision:.4f} - Val Recall: {val_recall:.4f} - Val F1: {val_f1:.4f}")
         
         print("-" * 50)
         print("Training Complete!")
